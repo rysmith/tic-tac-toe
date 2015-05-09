1965c26c71b28390a06362e50f6c93401690c71d
rysmith <ryansmith05@gmail.com>
2015-05-08 21:26:30 -0700

    update playerMover()

--- js/BoardController.js
+++ js/BoardController.js
@@ -5,71 +5,68 @@ function BoardController() {
 
     // get the array of the tic-tac-toe board
     this.gameBoard = [
-        {location: 'topLeft', content: null, value: null},
-        {location: 'topMiddle', content: null, value: null},
-        {location: 'topRight', content: null, value: null},
-        {location: 'centerLeft', content: null, value: null},
-        {location: 'centerMiddle', content: null, value: null},
-        {location: 'centerRight', content: null, value: null},
-        {location: 'bottomLeft', content: null, value: null},
-        {location: 'bottomMiddle', content: null, value: null},
-        {location: 'bottomRight', content: null, value: null}
+        {location: 'topLeft', row: 1, column: 1, content: null, value: 1, class: "first-row first-col"},
+        {location: 'topMiddle', row: 1, column: 2, content: null, value: 1,  class: "first-row second-col"},
+        {location: 'topRight', row: 1, column: 3, content: null, value: 0,  class: "first-row third-col"},
+        {location: 'centerLeft', row: 2, column: 1, content: null, value: 0,  class: "second-row first-col"},
+        {location: 'centerMiddle', row: 2, column: 2,  content: null, value: 0,  class: "second-row second-col"},
+        {location: 'centerRight', row: 2, column: 3,  content: null, value: 0,  class: "second-row third-col"},
+        {location: 'bottomLeft', row: 3, column: 1,  content: null, value: 0,  class: "third-row first-col"},
+        {location: 'bottomMiddle', row: 3, column: 2,  content: null, value: 0,  class: "third-row second-col"},
+        {location: 'bottomRight', row: 3, column: 3,  content: null, value: 0,  class: "third-row third-col"}
     ];
 
-    var winner;
+    var winner = null;
     var playerOneTurn = true;
 
-    //TODO(ryan): check if playerMove logic is working correctly
     this.playerMove = playerMove;
-    function playerMove () {
+    function playerMove (location) {
 
-        while (winner === false) {
+        if (this.gameBoard[location].value === 0) {
 
-            if (this.gameBoard.value === null) {
+            if (playerOneTurn) {
+                // playerOne's turn
+                this.gameBoard[location].content = 'x';
+                this.gameBoard[location].value = 1;
+                playerOneTurn = false;
 
-                if (playerOneTurn) {
+                //TODO(ryan):complete check for winner logic for playerOne
+                //if (this.checkForWinner() === "playerOne") {
+                //end game in favor of playerOne
+                //} else {
+                // move along, nothing to see here...
+                //}
 
-                    // player one's turn
-                    this.gameBoard.value = 1;
-                    if (this.checkForWinner() === "playerOne") {
-                        console.log(this.checkForWinner());
-
-                    } else {
-
-                        playerOneTurn = false;
-                    }
-
-                } else {
-
-                    //player two's turn
-                    this.gameBoard.value = -1;
-
-                    if (this.checkForWinner() === "player2") {
-
-                        console.log(this.checkForWinner());
-
-                    } else {
-
-                        playerOneTurn = true;
-                    }
-                }
             } else {
-                // this should run if you click on a square that has been played already
-                console.log("this square has been played");
+
+                //playerTwo's turn
+                this.gameBoard[location].content = 'o';
+                this.gameBoard[location].value = -1;
+                playerOneTurn = true;
+
+                //TODO(ryan):complete check for winner logic for playerTwo
+                //if (this.checkForWinner() === "playerTwo") {
+                //end game in favor of playerTwo
+                //} else {
+                // move along, nothing to see here...
+                //}
             }
+        } else {
+            // this should run if you click on a square that has been played already
+            console.log("this square has been played");
         }
     }
 
 
-    //TODO(ryan): write business logic to check if there is a winner
+    //TODO(ryan): write logic to check if there is a winner
     this.checkForWinner = checkForWinner;
     function checkForWinner () {
-        // check if there is a winner
+        for (var i = 0; i < this.gameBoard.length; i++) {
+            this.gameBoard[i].value
 
-        return winner;
+        }
+        //should return 'playerOne', 'playerTwo', or 'null'
+        //return winner;
     }
 
 }
-
-
-
